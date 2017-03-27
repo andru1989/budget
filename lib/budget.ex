9470e1b4057e129ea5fdc68bddda2cc109be3e68
Andres Vizcaino <andruvizcaino1989@gmail.com>
2017-03-27 00:28:34 -0500

    parse and filter rows

--- lib/budget.ex
+++ lib/budget.ex
@@ -1,5 +1,18 @@
 defmodule Budget do
+  alias NimbleCSV.RFC4180, as: CSV
+
   def list_transactions do
-    "Listing transactions"
+    File.read!("lib/transactions.csv")
+    |> parse
+    |> filter
+  end
+
+  defp parse(string) do
+    CSV.parse_string(string)
+  end
+
+  defp filter(rows) do
+    #Enum.map(rows, fn(row) -> Enum.drop(row, 1) end)
+    Enum.map(rows, &Enum.drop(&1, 1))
   end
 end
