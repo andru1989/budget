6a9ae886880da6bd56c9f0fdc204cef43f341787
Andres Vizcaino <andruvizcaino1989@gmail.com>
2017-03-27 00:50:31 -0500

    sort by amount and add print format

--- lib/budget.ex
+++ lib/budget.ex
@@ -6,6 +6,8 @@ defmodule Budget do
     |> parse
     |> filter
     |> normalize
+    |> sort
+    |> print
   end
 
   defp parse(string) do
@@ -31,5 +33,22 @@ defmodule Budget do
     string
     |> String.to_float
     |> abs
+  end
+
+  defp sort(rows) do
+    Enum.sort(rows, &sort_asc_by_amount(&1, &2))
+  end
+
+  defp sort_asc_by_amount([_, _, prev], [_, _, next]) do
+    prev < next
+  end
+
+  defp print(rows) do
+    IO.puts "\nTransactions:"
+    Enum.each(rows, &print_to_console(&1))
+  end
+
+  defp print_to_console([date, description, amount]) do
+    IO.puts "#{date} #{description} \t$#{:erlang.float_to_binary(amount, decimals: 2)}"
   end
 end
